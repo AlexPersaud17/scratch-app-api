9d43fad89fbca9dc5b3db040b3329dca828b4ad0
Alex Persaud <alex_persaud01@yahoo.com>
2017-07-19 13:51:37 -0400

    Create get method

--- .webpack/get.js
+++ .webpack/get.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 6);
+/******/ 	return __webpack_require__(__webpack_require__.s = 8);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -153,7 +153,9 @@ function buildResponse(statusCode, body) {
 }
 
 /***/ }),
-/* 6 */
+/* 6 */,
+/* 7 */,
+/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -174,56 +176,55 @@ var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
 var main = exports.main = function () {
   var _ref = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee(event, context, callback) {
-    var data, params, result;
+    var params, result;
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            data = JSON.parse(event.body);
             params = {
               TableName: 'scratch',
-              Item: {
+              // 'Key' defines the partition key and sort key of the item to be retrieved
+              // - 'userId': federated identity ID of the authenticated user
+              // - 'noteId': path parameter
+              Key: {
                 userId: event.requestContext.authorizer.claims.sub,
-                noteId: _uuid2.default.v1(),
-                content: data.content,
-                attachment: data.attachment,
-                createdAt: new Date().getTime()
+                noteId: event.pathParameters.id
               }
             };
-            _context.prev = 2;
-            _context.next = 5;
-            return dynamoDbLib.call('put', params);
+            _context.prev = 1;
+            _context.next = 4;
+            return dynamoDbLib.call('get', params);
 
-          case 5:
+          case 4:
             result = _context.sent;
 
-            callback(null, (0, _responseLib.success)(params.Item));
-            _context.next = 13;
+            if (result.Item) {
+              // Return the retrieved item
+              callback(null, (0, _responseLib.success)(result.Item));
+            } else {
+              callback(null, (0, _responseLib.failure)({ status: false, error: 'Item not found.' }));
+            }
+            _context.next = 11;
             break;
 
-          case 9:
-            _context.prev = 9;
-            _context.t0 = _context['catch'](2);
+          case 8:
+            _context.prev = 8;
+            _context.t0 = _context['catch'](1);
 
-            console.log(_context.t0);
             callback(null, (0, _responseLib.failure)({ status: false }));
 
-          case 13:
+          case 11:
           case 'end':
             return _context.stop();
         }
       }
-    }, _callee, this, [[2, 9]]);
+    }, _callee, this, [[1, 8]]);
   }));
 
   return function main(_x, _x2, _x3) {
     return _ref.apply(this, arguments);
   };
 }();
-
-var _uuid = __webpack_require__(7);
-
-var _uuid2 = _interopRequireDefault(_uuid);
 
 var _dynamodbLib = __webpack_require__(3);
 
@@ -236,12 +237,6 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 ;
-
-/***/ }),
-/* 7 */
-/***/ (function(module, exports) {
-
-module.exports = require("uuid");
 
 /***/ })
 /******/ ])));
